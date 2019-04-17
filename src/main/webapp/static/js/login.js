073283a584c7baa6a00439f2e9c06e068d5d73fa
qian.chen <2577342103@qq.com>
2019-04-17 18:00:57 +0800

    登陆页面修改

--- src/main/webapp/static/js/login.js
+++ src/main/webapp/static/js/login.js
@@ -349,3 +349,14 @@ function phoneCode(){
         }
     }
 }
+// window.onload=function() {
+//     function fixRem() {
+//         var windowWidth = document.documentElement.clientWidth || window.innerWidth || document.body.clientWidth;
+//         // windowWidth = windowWidth > 750 ? 750 : windowWidth;
+//         var rootSize = 28 * (windowWidth / 375);
+//         var htmlNode = document.getElementsByTagName("html")[0];
+//         htmlNode.style.fontSize = rootSize + 'px';
+//     }
+//     fixRem();
+//     window.addEventListener('resize', fixRem, false);
+// }
