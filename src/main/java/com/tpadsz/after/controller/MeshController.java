300f69bca9c05c816bbeda9c3ee6298c24ce531f
hongjian.chen <766256898@qq.com>
2019-05-10 15:10:01 +0800

    ...

--- src/main/java/com/tpadsz/after/controller/MeshController.java
+++ src/main/java/com/tpadsz/after/controller/MeshController.java
@@ -66,6 +66,7 @@ public class MeshController {
         map.put("id", dict.getId());
         map.put("uid", dict.getUid());
         map.put("name", dict.getName());
+        map.put("projectId",dict.getId());
         try {
             meshService.save(map);
         } catch (RepetitionException e) {
