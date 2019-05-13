95318d124f1084b1f8c25a1cf6ccd8a691859e43
qian.chen <2577342103@qq.com>
2019-05-13 17:06:19 +0800

    场景列表提交

--- src/main/webapp/static/js/scene.js
+++ src/main/webapp/static/js/scene.js
@@ -2,27 +2,59 @@
  * Created by qian.chen on 2019/5/10.
  */
 $(function () {
+    var accountNum=0;
+    $('tbody .checkbox input').each(function () {
+        if($(this).prop('checked')){
+            accountNum++;
+        }
+    })
+    $('.amount').text(accountNum);
+    //复选框监听--监听全选
+    $('#all').click(function () {
+        var checked = $(this).prop('checked');
+        if (checked) {
+            var length = $('tbody tr').length;
+            accountNum = length;
+            $('tbody .checkbox input').each(function () {
+                $(this).prop('checked', true);
+            })
+        } else {
+            accountNum = 0;
+            $('tbody .checkbox input').each(function () {
+                $(this).prop('checked', false);
+            })
+        }
+        $('.amount').text(accountNum);
+    })
+    //复选框监听--监听单选
+    $('tbody .checkbox input').click(function () {
+        var allChecked = true;
+        var checked = $(this).prop('checked');
+        if (checked) {
+            accountNum++;
+        } else {
+            accountNum--;
+        }
+        $('tbody .checkbox input').each(function () {
+            var otherChecked = $(this).prop('checked');
+            if (!otherChecked) {
+                allChecked = false;
+            }
+        })
+        $('.amount').text(accountNum);
+        if (!allChecked) {
+            $('#all').prop('checked', false);
+        } else {
+            $('#all').prop('checked',true );
+        }
+    })
+})
+
+$(function () {
     var tabs = "meshList";
     var index = 0;
     left(tabs, index);
     var deleteArray=[];
-
-    $("#multiDel").click(function () {
-        var ids = [];
-        $('input[name="ids"]:checked').each(function () {
-            ids.push($(this).val());
-        });
-        if (ids.length > 0) {
-            $('div[openContent="delete-place"]').addClass('active');
-            var width = document.body.scrollWidth;
-            var height = document.body.scrollHeight;
-            $('.hide-iframe').addClass('active');
-            $('.hide-iframe').css({
-                'width': width,
-                'height': height
-            });
-        }
-    });
     //单选重命名
     var id;
     $('.reset-name').click(function () {
@@ -84,6 +116,7 @@ $(function () {
     }
     //单选删除
     $('.singleDel').click(function () {
+        var deleteArray=[];
         // ids = $(this).parent().siblings('.checkbox').find('input').val();
         var  sid=parseInt($(this).parent().siblings('.sid').find('input').val());
         var sceneId=parseInt($(this).parent().siblings('.sceneId').text());
@@ -111,33 +144,51 @@ $(function () {
         console.log(newJsonArray);
         $('div[openContent="delete-place"]').removeClass('active');
         $('.hide-iframe').removeClass('active');
-        $.ajax({
-            type: "POST",
-            url: "/alink-hq/scene/delete",
-            data: {sceneInfo: newJsonArray},
-            dataType: "json",
-            success: function (res) {
-                if (res.result == '000') {
-                    location.reload();
-                    $('tbody tr td.checkbox input').prop('checked',false);
-                    $('#all').prop('checked',false);
-                } else if (res.result == '200') {
-                    console.log('系统错误');
-                }
+        deleteScene(newJsonArray);
+    })
+    //多选删除
+    $("#multiDel").click(function () {
+         deleteArray=[];
+        $('input[name="sid"]:checked').each(function () {
+            var sid=parseInt($(this).val());
+            var sceneId=parseInt($(this).parent().siblings('.sceneId').text());
+            var msg={
+                id:sid,
+                sceneId:sceneId
             }
-        })
-    });
-
-    // $('.pop-btn .yes').click(function () {
-    //     console.log("ids=" + ids);
-    //     deletePlace(ids);
-    // })
-    // function deletePlace(ids) {
-    //     if (ids) {
-    //         location.href = "/alink-hq/place/move?ids=" + ids;
-    //     }
-    // }
-});
+            deleteArray.push(msg);
+            console.log(deleteArray);
+        });
+        if (deleteArray.length > 0) {
+            $('div[openContent="delete-place"]').addClass('active');
+            var width = document.body.scrollWidth;
+            var height = document.body.scrollHeight;
+            $('.hide-iframe').addClass('active');
+            $('.hide-iframe').css({
+                'width': width,
+                'height': height
+            })
+        }
+    })
+})
+function deleteScene(newJsonArray){
+    $.ajax({
+        type: "POST",
+        url: "/alink-hq/scene/delete",
+        data: {sceneInfo: newJsonArray},
+        dataType: "json",
+        success: function (res) {
+            console.log(res);
+            if (res.result == '000') {
+                location.reload();
+                $('tbody tr td.checkbox input').prop('checked',false);
+                $('#all').prop('checked',false);
+            } else if (res.result == '200') {
+                console.log('系统错误');
+            }
+        }
+    })
+}
 //重命名校验
 function nameKeyUp() {
     var rename = $('#rename').val();
