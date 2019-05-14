61afc204e6017d7173654f75cf2156a3c5f16d45
chenhao.lu <chenhao.lu>
2019-05-14 18:18:09 +0800

    增加token拦截器

---
+++ src/main/java/com/tpadsz/after/realm/UserTokenValidationInterceptor.java
@@ -0,0 +1,42 @@
+package com.tpadsz.after.realm;
+
+import com.tpadsz.after.constants.MemcachedObjectType;
+import com.tpadsz.after.entity.User;
+import net.rubyeye.xmemcached.MemcachedClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class UserTokenValidationInterceptor extends HandlerInterceptorAdapter {
+
+    @Autowired
+    private MemcachedClient client;
+
+
+    @Override
+    public boolean preHandle(HttpServletRequest request,
+                             HttpServletResponse response, Object handler) throws Exception {
+        try {
+            HttpSession session = request.getSession();
+            User loginUser = (User) session.getAttribute("user");
+            String uid = loginUser.getId();
+            String key = MemcachedObjectType.CACHE_TOKEN.getPrefix() + uid;
+            String expected = (String) client.get(key);
+            if("disabled".equals(expected)){
+                String url = "/alink-hq/";
+                response.sendRedirect(url);
+                return false;
+            }
+        }catch (Exception e){
+
+        }
+        return true;
+    }
+
+}
