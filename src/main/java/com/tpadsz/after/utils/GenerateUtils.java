52c015ddb2d26e46222c44a8f371ffb199564c22
hongjian.chen <766256898@qq.com>
2019-05-15 10:06:29 +0800

    新增token验证

--- src/main/java/com/tpadsz/after/utils/GenerateUtils.java
+++ src/main/java/com/tpadsz/after/utils/GenerateUtils.java
@@ -1,7 +1,7 @@
 package com.tpadsz.after.utils;
 
-import com.tpadsz.after.constants.MemcachedObjectType;
 import com.tpadsz.after.exception.SystemAlgorithmException;
+
 import java.util.Random;
 import java.util.UUID;
 
@@ -18,7 +18,7 @@ public class GenerateUtils {
         String reg = "^.*\\d{3}.*$";
         Random random = new Random();
         for (int i = 0; i < length; i++) {
-// 输出字母还是数字
+            // 输出字母还是数字
             if (num1 == 5) {
                 charOrNum = "num";
             } else if (num2 == 3) {
@@ -36,29 +36,29 @@ public class GenerateUtils {
             // 字符串
             if ("char".equalsIgnoreCase(charOrNum)) {
                 // 取得大写字母还是小写字母
-//                int choice = random.nextInt(2) % 2 == 0 ? 65 : 97;
+                // int choice = random.nextInt(2) % 2 == 0 ? 65 : 97;
                 do {
                     randomValue = 65 + random.nextInt(26);
                     char[] arr = val.toCharArray();
                     for (int j = 0; j < arr.length; j++) {
-                        if(randomValue==arr[j]){
+                        if (randomValue == arr[j]) {
                             isDuplicateNum++;
                         }
                     }
-                } while (isDuplicateNum==2);
+                } while (isDuplicateNum == 2);
                 val += (char) randomValue;
                 num1++;
             } else if ("num".equalsIgnoreCase(charOrNum)) { // 数字
                 randomValue = random.nextInt(10);
-                if(num2==2){
+                if (num2 == 2) {
                     do {
                         String[] arr = val.split("");
                         for (int j = 0; j < arr.length; j++) {
-                            if(String.valueOf(randomValue).equals(arr[j])){
+                            if (String.valueOf(randomValue).equals(arr[j])) {
                                 isDuplicateNum++;
                             }
                         }
-                    } while (isDuplicateNum==2);
+                    } while (isDuplicateNum == 2);
                 }
                 val += String.valueOf(randomValue);
                 num2++;
@@ -69,12 +69,12 @@ public class GenerateUtils {
 
 
     public static boolean check(String str) {
-        char[] arr=str.toCharArray();
+        char[] arr = str.toCharArray();
         boolean flag = true;
         for (int i = 1; i < arr.length - 1; i++) {
-            int firstIndex = (int)arr[i-1];
-            int secondIndex = (int)arr[i];
-            int thirdIndex = (int)arr[i+1];
+            int firstIndex = (int) arr[i - 1];
+            int secondIndex = (int) arr[i];
+            int thirdIndex = (int) arr[i + 1];
             if ((thirdIndex - secondIndex == 1) && (secondIndex - firstIndex == 1)) {
                 flag = false;
             }
@@ -94,7 +94,7 @@ public class GenerateUtils {
 
 
     public static String generateToken() throws SystemAlgorithmException {
-        String token = null;
+        String token;
         try {
             token = UUID.randomUUID().toString().replaceAll("-", "");
         } catch (Exception e) {
