581608f421f79de681c1f57f18f4781cf294719c
qian.chen <2577342103@qq.com>
2019-05-22 15:29:50 +0800

    页面滚动条修改

--- src/main/webapp/static/js/left.js
+++ src/main/webapp/static/js/left.js
@@ -2,9 +2,12 @@
  * Created by qian.chen on 2019/5/6.
  */
 $(function(){
+    // var scroll=$('.c-container').height();
+    // console.log('scroll',scroll);
+    // $('html').css({'height':scroll,'overflow-y':'hidden'})
     var height=$(document).height();
     $('.main-left').css('height',height);
-    $('html').css({'overflow-y':'visible','height':height});
+    // $('html').css({'overflow-y':'visible','height':height});
     $('.on-off-triangle').click(function () {
         var imgUrl = $(this).attr('src');
         if(imgUrl == '/alink-hq/static/img/left-reduce.png') {
