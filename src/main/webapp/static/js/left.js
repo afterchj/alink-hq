5fe0a8dc9768e79f05d8e8c34538e30724548694
qian.chen <2577342103@qq.com>
2019-05-23 11:35:57 +0800

    项目列表修改

--- src/main/webapp/static/js/left.js
+++ src/main/webapp/static/js/left.js
@@ -21,7 +21,6 @@ $(function(){
     })
 
     // $(document).scroll(function() {
-    //
     //         var scroH = $(document).scrollTop();  //滚动高度
     //         var viewH = $(window).height();  //可见高度
     //         var contentH = $(document).height();  //内容高度
