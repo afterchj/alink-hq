d9e39c9eb7b11005aa734272c1d144c8f3d077b6
hongjian.chen <766256898@qq.com>
2019-06-03 17:45:40 +0800

    合作管理禁用接口本地开发

--- src/main/java/com/tpadsz/after/service/CooperateService.java
+++ src/main/java/com/tpadsz/after/service/CooperateService.java
@@ -2,6 +2,7 @@ package com.tpadsz.after.service;
 
 import com.tpadsz.after.entity.CooperationInfo;
 import com.tpadsz.after.entity.SearchDict;
+import com.tpadsz.after.exception.RepetitionException;
 
 import java.util.List;
 import java.util.Map;
@@ -14,6 +15,8 @@ public interface CooperateService {
     List<Map> getByMap(SearchDict dict);
 
     CooperationInfo getCooperationInfo(int id);
+
+    int getCount(int id);
 
     void save(CooperationInfo info);
 
