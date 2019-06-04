7e53183ed3b43886d54bf889d02dcbfedb1fbb86
yuanjie.fang <Tp123456>
2019-06-04 10:12:55 +0800

    合作列表弹框交互

--- src/main/webapp/static/js/cooperateList.js
+++ src/main/webapp/static/js/cooperateList.js
@@ -2,6 +2,9 @@
  * Created by qian.chen on 2019/5/27.
  */
 $(function () {
+    var width = window.screen.width;
+    var height = window.screen.height;
+    var companyId,status;
     //如果备忘录为空时
     // $('.memo-edit').click(function(){
     //     $('div[openContent="memo-edit"]').addClass('active');
@@ -18,27 +21,36 @@ $(function () {
     //     $('.hide-iframe').removeClass('active');
     // })
     //如果备忘录不为空时
-    $('.memo-edit').click(function () {
-        $(this).parent('td').addClass('active');
-    });
+    // $('.memo-edit').click(function () {
+    //
+    // });
     // $('.meno-nav').mouseout(function () {
     //     console.log("show",false);
     //     $(this).parent("div").css("display","none");
     // });
-    $('.meno-nav').click(function () {
-        $('div[openContent="memo-edit"]').addClass('active');
-        var width = window.screen.width;
-        var height = window.screen.height;
-        $('.hide-iframe').addClass('active');
-        $('.hide-iframe').css({
-            'width': width,
-            'height': height
-        })
+    $('.memo-edit').click(function () {
+        var contentText=$(this).find('.memo-content').text();
+        console.log('contentText',contentText);
+        if(contentText==''){
+            //如果备忘录为空时
+            $('div[openContent="memo-edit"]').addClass('active');
+            $('.hide-iframe').addClass('active');
+            $('.hide-iframe').css({
+                'width': width,
+                'height': height
+            })
+        }else{
+            //如果备忘录不为空时
+            $(this).parent('td').addClass('active');
+            $(this).parent('td').siblings('td').removeClass('active');
+        }
+
     })
     $('div[openContent="memo-edit"] button.reduce').click(function () {
         $('div[openContent="memo-edit"]').removeClass('active');
         $('.hide-iframe').removeClass('active');
     })
+
     //启用禁用
     $('.off-or-on-coo').click(function () {
         $('div[openContent="start-use"]').addClass('active');
@@ -49,25 +61,34 @@ $(function () {
             'width': width,
             'height': height
         })
-
-        //如果已经禁用，则启用
-        var companyName = $(this).siblings('.coo-name').text();
-        console.log(companyName)
-        $('div[openContent="start-use"] .off-or-on').text('您确定要启用' + companyName + '？')
-        //如果已经启用，则禁用
-        var content = '<div>您确定要禁用' + companyName + '？</div><p style="color: red; font-size: 14px; font-weight: normal;margin-top: 20px;">禁用后，其名下所有账户将无法使用，请慎重！</p>'
-        $('div[openContent="start-use"] .off-or-on').html(content);
+        var companyName = $(this).parent().siblings('.coo-name').text();
+         status=parseInt($(this).attr('alt'));
+         companyId = parseInt($(this).parent().siblings('.coo-name').find('a').attr('alt'));
+        // console.log('status',status,'companyName',companyName);
+        if(status==0){
+            //如果已经启用，则禁用
+            var content = '<div>您确定要禁用' + companyName + '？</div><p style="color: red; font-size: 14px; font-weight: normal;margin-top: 20px;">禁用后，其名下所有账户将无法使用，请慎重！</p>'
+            $('div[openContent="start-use"] .off-or-on').html(content);
+        }else if(status==1){
+            //如果已经禁用，则启用
+            $('div[openContent="start-use"] .off-or-on').text('您确定要启用' + companyName + '？')
+        }
     })
     $('div[openContent="start-use"] button.reduce').click(function () {
         $('div[openContent="start-use"]').removeClass('active');
         $('.hide-iframe').removeClass('active');
     })
+    $('div[openContent="start-use"] button.yes').click(function () {
+        $('div[openContent="start-use"]').removeClass('active');
+        $('.hide-iframe').removeClass('active');
+        location.href = "/alink-hq//cooperate/saveUpdate?id=" + companyId+'&status='+status;
+    })
 
         //重命名
         $('.reset-name').click(function () {
             $('div[openContent="reset-name"]').addClass('active');
-            var width = window.screen.width;
-            var height = window.screen.height;
+            // var width = window.screen.width;
+            // var height = window.screen.height;
             $('.hide-iframe').addClass('active');
             $('.hide-iframe').css({
                 'width': width,
@@ -87,8 +108,8 @@ $(function () {
        //删除公司信息
        $('.delete-project').click(function(){
            $('div[openContent="delete-project"]').addClass('active');
-           var width = window.screen.width;
-           var height = window.screen.height;
+           // var width = window.screen.width;
+           // var height = window.screen.height;
            $('.hide-iframe').addClass('active');
            $('.hide-iframe').css({
                'width': width,
