58c90c7f2d79e35a6844ce3d7f01729d9f73afb1
李楠楠 <nannan.li@tpadsz.cn>
2019-07-04 11:16:34 +0800

    定时全开全关数据库记录为-1、-2

--- src/main/java/com/tpadsz/after/controller/RolePermissionInfoController.java
+++ src/main/java/com/tpadsz/after/controller/RolePermissionInfoController.java
@@ -47,6 +47,7 @@ public class RolePermissionInfoController {
 //        model.addAttribute("users",users);
 //        model.addAttribute("rolePermissions",rolePermissions);
         model.addAttribute("rolePermissionList",rolePermissionList);
+        model.addAttribute("test","test2");
         return "roleManage/roleDetail";
     }
 
