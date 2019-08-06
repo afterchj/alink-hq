d904782a645de9b7c3777b8964d02cfd930a3358
李楠楠 <nannan.li@tpadsz.cn>
2019-08-06 10:00:10 +0800

    定时详情

--- src/main/webapp/static/js/timerDetail.js
+++ src/main/webapp/static/js/timerDetail.js
@@ -1,3 +1,4 @@
+var lightInfo2;
 $(function () {
     var header = '<div class="scene-msg  timer f-l" id="time-scene" style=" width: 960px;"><div class="scene-content" ><div class="scene-list clearfix"><div class="f-l"><span>场景下的区域信息</span><span style="color:#999;font-weight: 300;">>></span></div><div class="f-l"><span>该区域下的组信息</span><span style="color:#999;font-weight: 300;">>></span></div><div class="f-l"> <span>该组下的灯详情</span></div></div><div class="clearfix  time-scene-msg  scene-msg-list">';
     $("#timeTitle .sname-a").click(function () {
@@ -18,6 +19,7 @@ $(function () {
                 var groupList = msg.groupList;
                 var lightList = msg.lightList;
                 var lightInfo = msg.lightInfo;
+                lightInfo2=lightInfo;
                 var scene = '';
                 var place = '';
                 var group = '';
@@ -45,49 +47,55 @@ $(function () {
                     }
                 });
                 place += '</ul></div>';
-                group += '<div class="f-l" style="border-right: none;" id="groupXYList"><ul>';
-
+                group += '<div class="f-l" style="border-right: none;" id="groupMsg"><ul>';
                 $.each(groupList, function (key, value) {
                     if (isEmpty(value.x)) {
                         if (key == 0) {
-                            group += '<li class="gid active" alt="' + value.gid + '"><span >' + value.gname + '</span>(<span class="groupXy">查看详情</span>)</li>';
+                            group += '<li class="gid active" alt="' + value.gid + '"><span >' + value.gname + '</span>(<span>查看详情</span>)</li>';
                         } else {
-                            group += '<li class="gid" alt="' + value.gid + '"><span >' + value.gname + '</span>(<span class="groupXy">查看详情</span>)</li>';
+                            group += '<li class="gid" alt="' + value.gid + '"><span >' + value.gname + '</span>(<span>查看详情</span>)</li>';
                         }
                     } else {
                         if (key == 0) {
-                            group += '<li class="gid active" alt="' + value.gid + '"><span >' + value.gname + '</span>(<span class="groupXy">' + value.x + ',' + value.y + '</span>)</li>';
+                            group += '<li class="gid active" alt="' + value.gid + '"><span >' + value.gname + '</span>(<span>' + value.x + ',' + value.y + '</span>)</li>';
                         } else {
-                            group += '<li class="gid" alt="' + value.gid + '"><span >' + value.gname + '</span>(<span class="groupXy">' + value.x + ',' + value.y + '</span>)</li>';
+                            group += '<li class="gid" alt="' + value.gid + '"><span >' + value.gname + '</span>(<span>' + value.x + ',' + value.y + '</span>)</li>';
                         }
                     }
                 });
 
                 group += '</ul></div>';
-                light += '<div class="f-l clearfix"><div class="f-l"><ul class="one">';
+                light += '<div class="f-l clearfix"><div class="f-l" id="lightMsg"><ul class="one">';
                 // lightXY += '<div class="f-l"><ul class="two">';
-                var lightSpan1 = '<span>' + value.lname + '(' + value.lmac + ')</span><span>' + value.x + '(色温),' + value.y + '(亮度)</span></li>';
-                var lightSpan2 = '<span>' + value.lname + '(' + value.lmac + ')</span><span>' + value.x + ',' + value.y + '</span></li>';
+                // var lightSpan1 = '';
+                // var lightSpan2 = '';
                 $.each(lightList, function (key, value) {
-
+                    // lightSpan1 = '<span>' + value.lname + '(' + value.lmac + ')</span><span>' + value.x + '(色温),' + value.y + '(亮度)</span></li>';
+                    // lightSpan2 = '<span>' + value.lname + '(' + value.lmac + ')</span><span>' + value.x + ',' + value.y + '</span></li>';
                     if (key == 0 && lightInfo == null) {
                         light += '<li class="lid active" alt="'+value.id+'">';
-                        light =light+lightSpan1;
+                        light+= '<span>' + value.lname + '（' + value.lmac + '）</span><span>' + value.x + '（色温）,' + value.y + '（亮度）</span></li>';
+                        // light =light+lightSpan1;
                     } else if (key != 0 && lightInfo == null) {
                         light += '<li class="lid" alt="'+value.id+'">';
-                        light =light+lightSpan2;
+                        light += '<span>' + value.lname + '（' + value.lmac + '）</span><span>' + value.x + ',' + value.y + '</span></li>';
+                            // light =light+lightSpan2;
                     } else if (key == 0 && lightInfo != null && value.id == lightInfo.id) {
-                        light += '<li class="lid active">' + value.lname + '(' + value.lmac + ')';
-                        light =light+lightSpan1;
+                        light += '<li class="lid active">';
+                        light+= '<span>' + value.lname + '（' + value.lmac + '）</span><span>' + value.x + '（色温),' + value.y + '（亮度）</span></li>';
+                        // light =light+lightSpan1;
                     } else if (key != 0 && lightInfo != null && value.id == lightInfo.id) {
                         light += '<li class="lid active">';
-                        light =light+lightSpan2;
+                        light += '<span>' + value.lname + '（' + value.lmac + '）</span><span>' + value.x + ',' + value.y + '</span></li>';
+                        // light =light+lightSpan2;
                     } else if (key == 0 && lightInfo != null && value.id != lightInfo.id) {
                         light += '<li class="lid">';
-                        light =light+lightSpan1;
+                        light+= '<span>' + value.lname + '（' + value.lmac + '）</span><span>' + value.x + '（色温）,' + value.y + '（亮度）</span></li>';
+                        // light =light+lightSpan1;
                     } else if (key != 0 && lightInfo != null && value.id != lightInfo.id) {
-                        light += '<li class="lid">' + value.lname + '(' + value.lmac + ')</li>';
-                        light =light+lightSpan2;
+                        light += '<li class="lid">';
+                        light += '<span>' + value.lname + '（' + value.lmac + '）</span><span>' + value.x + ',' + value.y + '</span></li>';
+                        // light =light+lightSpan2;
                     }
 
                     // if (key == 0 && lightInfo == null) {
@@ -125,36 +133,7 @@ $(function () {
 $('.p-content ').on('click', 'li', function () {
     $(this).toggleClass('active').siblings().removeClass('active');
 })
-$('.p-content ').on('click', '.gid', function () {
-    var gid = $(this).attr('alt');
-    var sid = $(".ssid").val();
-    $('.one li').remove();
-    $('.two li').remove();
-    var content = '';
-    var content1 = '';
-    $.ajax({
-        type: "post",
-        url: "/alink-hq/scene/groupDetail",
-        data: {
-            "gid": gid,
-            "sid": sid
-        },
-        async: true,
-        success: function (res) {
-            var lightList = res.lightList;
-            $.each(lightList, function (key, value) {
-                content += '<li class="lid" alt=' + value.id + '>' + value.lname + '(' + value.lmac + ')' + '</li>';
-                if (key == 0) {
-                    content1 += '<li class="lid" alt="' + value.id + '" style="text-align: center;">' + value.x + '(色温)，' + value.y + '(亮度)</li>';
-                } else {
-                    content1 += '<li class="lid" alt="' + value.id + '" style="text-align: center;">' + value.x + '，' + value.y + '</li>';
-                }
-            })
-            $('.one').append(content);
-            $('.two').append(content1);
-        }
-    })
-})
+
 
 function isEmpty(value) {
     if (value == null || value == "" || value == "undefined" || value == undefined) {
@@ -176,3 +155,96 @@ $(function () {
         $(".time .f-l:eq(1)>span").append('<a style="pointer-events: none">查看项目详情</a>')
     }
 })
+
+// $(function () {
+// //        var length = $('.scene-msg .scene-msg-list>div:nth-child(3)>div:nth-child(1)  ul').height();
+// //        if (length > 380) {
+// //            $('.scene-msg .scene-msg-list>div:nth-child(3)>div:nth-child(1) ul').addClass('active');
+// //            $('.scene-msg .scene-msg-list>div:nth-child(3)>div:nth-child(1)').css('border-right', 'none')
+// //        }
+// //     var lightInfo=[[${lightInfo}]];
+//     console.log(lightInfo2);
+//     if(lightInfo2!=null){
+//         $('#groupMsg li').each(function(){
+//             var gidss=parseInt($(this).attr('alt'));
+//             var lgid=lightInfo2.gid;
+//             console.log('lgid',lgid)
+//             if(gidss==lgid){
+//                 $(this).addClass('active');
+//             }else{
+//                 $(this).removeClass('active')
+//             }
+//         })
+//     }
+// });
+$('.p-content ').on('click', '.gid', function () {
+    var gid = $(this).attr('alt');
+//        var sid = $(".ssid").val();
+    var sid = GetUrlParam('sid');
+    // var lightInfo=[[${lightInfo}]];
+    // $('.two li').remove();
+    var content = '';
+    // var content1 = '';
+    $.ajax({
+        type: "post",
+        url: "/alink-hq/scene/groupDetail",
+        data: {
+            "gid": gid,
+            "sid": sid
+        },
+        async: true,
+        success: function (res) {
+            $('.one li').remove();
+            var lightList = res.lightList;
+            $.each(lightList, function (key, value) {
+                if(lightInfo2==null){
+                    if (key == 0) {
+                        content += '<li class="lid" alt="' + value.id + '" ><span>' + value.lname + '（' + value.lmac + '）' + '</span><span>' + value.x + '（色温），' + value.y + '（亮度）</span></li>';
+                    }else{
+                        content += '<li class="lid" alt="' + value.id + '" ><span>' + value.lname + '（' + value.lmac + '）' + '</span><span>' + value.x + '，' + value.y + '</span></li>';
+                    }
+                }else if(lightInfo2!=null && lightInfo2.id==value.id){
+                    if (key == 0) {
+                        content += '<li class="lid active" alt="' + value.id + '" ><span>' + value.lname + '（' + value.lmac + '）' + '</span><span>' + value.x + '（色温），' + value.y + '(亮度）</span></li>';
+                    }else{
+                        content += '<li class="lid active" alt="' + value.id + '" ><span>' + value.lname + '（' + value.lmac + '）' + '</span><span>' + value.x + '，' + value.y + '</span></li>';
+                    }
+                }else{
+                    if (key == 0) {
+                        content += '<li class="lid " alt="' + value.id + '" ><span>' + value.lname + '（' + value.lmac + '）' + '</span><span>' + value.x + '（色温），' + value.y + '(亮度）</span></li>';
+                    }else{
+                        content += '<li class="lid " alt="' + value.id + '" ><span>' + value.lname + '（' + value.lmac + '）' + '</span><span>' + value.x + '，' + value.y + '</span></li>';
+                    }
+                }
+                // content += '<li class="lid" alt=' + value.id + '>' + value.lname + '(' + value.lmac + ')' + '</li>';
+                // if (key == 0) {
+                //     content1 += '<li class="lid" alt="' + value.id + '" style="text-align: center;">' + value.x + '(色温)，' + value.y + '(亮度)</li>';
+                // } else {
+                //     content1 += '<li class="lid" alt="' + value.id + '" style="text-align: center;">' + value.x + '，' + value.y + '</li>';
+                // }
+            })
+            $('.one').append(content);
+            // $('.two').append(content1);
+        }
+    })
+})
+
+//paraName 等找参数的名称
+function GetUrlParam(paraName) {
+    var url = document.location.toString();
+    var arrObj = url.split("?");
+    if (arrObj.length > 1) {
+        var arrPara = arrObj[1].split("&");
+        var arr;
+        for (var i = 0; i < arrPara.length; i++) {
+            arr = arrPara[i].split("=");
+            if (arr != null && arr[0] == paraName) {
+                return arr[1];
+            }
+        }
+        return "";
+    }
+    else {
+        return "";
+    }
+}
