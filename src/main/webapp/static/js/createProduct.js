a9f03c6fbf9cab7107f36a44175e1a61cb9246dc
yuanjie.fang <Tp123456>
2019-08-14 13:42:06 +0800

    新增产品

--- src/main/webapp/static/js/createProduct.js
+++ src/main/webapp/static/js/createProduct.js
@@ -2,6 +2,15 @@
  * Created by yuanjie.fang on 2019/8/14.
  */
 $(function () {
+    //监听公司select选择
+    $('#belongCompany').change(function () {
+        var belongCompany=$(this).children('option:selected').val();
+        if(belongCompany==''){
+            $('#belongCompany').prev('.verify').text('请选择隶属公司');
+        }else{
+            $('#belongCompany').prev('.verify').text('');
+        }
+    })
    $('.submit button').click(function () {
        var productName=$('#productName').val();
        var productType=$('#productType').val();
@@ -17,30 +26,47 @@ $(function () {
        console.log('产品说明',productExplain);
 
         //同一个公司的产品名称、代码号（产品 ID）不能重复，若重复,提示已存在请重新输入
+       var isTrue1=false;
        if(productName==''){
            $('#productName').prev('.verify').text('请输入产品名称');
        }else if(productName=='重复'){
            $('#productName').prev('.verify').text('已存在，请重新输入');
        }else{
            $('#productName').prev('.verify').text('');
+           isTrue1=true;
        }
 
+       var isTrue2=false;
        if(productId==''){
            $('#productId').prev('.verify').text('请输入代码号（产品 ID）');
        }else if(productName=='重复'){
            $('#productId').prev('.verify').text('已存在，请重新输入');
        }else{
            $('#productId').prev('.verify').text('');
+           isTrue2=true;
        }
 
+       var isTrue3=false;
        if(belongCompany==''){
            $('#belongCompany').prev('.verify').text('请选择隶属公司');
        }else if(productName=='重复'){
            $('#belongCompany').prev('.verify').text('已存在，请重新输入');
        }else{
            $('#belongCompany').prev('.verify').text('');
+           isTrue3=true;
        }
 
+       console.log(isTrue1,isTrue2,isTrue3);
+        if(isTrue1 && isTrue2 && isTrue3){
+            //新增成功！
+            var selector = $('div[openContent="loading"]');
+            $('div[openContent="loading"] .title').text('新增成功！即将跳转产品列表');
+            selector.addClass('active');
+            showOverlay();
+            setTimeout(function () {
+                location.href="/alink-hq/productList";
+            },800)
+        }
+   })
 
-   }) 
 })
