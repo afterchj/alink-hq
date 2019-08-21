fbe70970aed3902b16d505a664922d285f0ea627
yuanjie.fang <Tp123456>
2019-08-21 15:51:57 +0800

    产品列表bug修改

--- src/main/webapp/static/js/OTAFile.js
+++ src/main/webapp/static/js/OTAFile.js
@@ -3,17 +3,18 @@
  */
 
 $(function () {
-    $('body').click(function () {
+    $('body').click(function (event) {
+        event.stopPropagation();
         $('.memo-edit').parent('td').removeClass('active');
     })
     var id;
     $('.singleDel').click(function () {
         id = $(this).attr("alt");
-        var fileName=$(this).parent().siblings('.otaName').text();
+        var fileName = $(this).parent().siblings('.otaName').text();
         var selector = $('div[openContent="delete-pop"]');
         selector.addClass('active');
-        $('div[openContent="delete-pop"] .reset-pwd p').text('您确定要删除 '+fileName+' 固件？');
-        $('div[openContent="delete-pop"] .reset-pwd-hint').text('删除后， '+fileName+' 固件所有历史版本将均被删除，请慎重！');
+        $('div[openContent="delete-pop"] .reset-pwd p').text('您确定要删除 ' + fileName + ' 固件？');
+        $('div[openContent="delete-pop"] .reset-pwd-hint').text('删除后， ' + fileName + ' 固件所有历史版本将均被删除，请慎重！');
         adjust(selector);
         showOverlay();
     });
@@ -36,18 +37,20 @@ $(function () {
     $('.memo-edit').click(function (event) {
         event.stopPropagation();
         id = $(this).find('.meno-img').attr('alt');
-        var isEmpty=$(this).find('.memo-content').text()==''?  true:false;
-        console.log('isEmpty',isEmpty);
-        console.log('我被点击了',$(this),'id',id);
+        var isEmpty = $(this).find('.memo-content').text() == '' ? true : false;
+        console.log('isEmpty', isEmpty);
+        console.log('我被点击了', $(this), 'id', id);
         //如果备忘录为空时
-        if(isEmpty){
+        if (isEmpty) {
             var selector = $('div[openContent="memo-edit"]');
             selector.addClass('active');
             $(".wishContent").val('');
             $(".wordsNum").html(checkStrLengths('', 200) + '/200');
+            $(this).parent('td').removeClass('active');
+            $(this).parent().parent('tr').siblings('tr').children('td').removeClass('active');
             adjust(selector);
             showOverlay();
-        }else{
+        } else {
             $(this).parent('td').addClass('active');
             $(this).parent().parent('tr').siblings('tr').children('td').removeClass('active');
         }
@@ -78,19 +81,21 @@ $(function () {
     //点击确定时
     $('div[openContent="memo-edit"] button.yes').click(function (event) {
         event.stopPropagation();
-        console.log('idds',id);
+        console.log('idds', id);
         var selector = $('div[openContent="memo-edit"]');
         selector.removeClass('active');
         hideOverlay();
         //获取输入内容
         var content = $(".wishContent").val();
-        console.log("content", content,'id',id);
+        console.log("content", content, 'id', id);
         $.ajax({
             type: "POST",
             url: "/alink-hq/file/saveUpdate",
-            data: {"id":id,"otaDesc": content},
+            data: {"id": id, "otaDesc": content},
             success: function (res) {
+                console.log('res',res);
                 if (res == 'ok') {
+
                     location.reload();
                 }
             }
